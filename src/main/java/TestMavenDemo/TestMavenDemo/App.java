97fd7d1849ab02c7a4bfb3c8de72f87103763422
ankit9009 <ankitjagtap9009@gmail.com>
2020-02-22 10:58:43 +0530

    this is my first project

---
+++ src/main/java/TestMavenDemo/TestMavenDemo/App.java
@@ -0,0 +1,13 @@
+package TestMavenDemo.TestMavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/TestMavenDemo/TestMavenDemo/ap.java
@@ -0,0 +1,10 @@
+package TestMavenDemo.TestMavenDemo;
+
+public class ap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello world");
+	}
+
+}
